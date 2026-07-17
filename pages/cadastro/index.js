e37190e9fff97b6b624b21cc66bb685a5f177fed
Thiago Antonio <thiagoacnascimento@gmail.com>
2026-07-17 13:25:40 -0400

    feat: implement `handleSubmit` function and confirm email page

--- pages/cadastro/index.js
+++ pages/cadastro/index.js
@@ -7,43 +7,65 @@ export default function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const requestBody = { username, email, password };
+    const response = await fetch("/api/v1/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    if (response.status === 201) {
+      location.href = "/cadastro/confirmar";
+    }
+  }
+
   return (
     <>
       <h1>Cadastro</h1>
-      <div>
-        Nome do usuario:
-        <input
-          type="text"
-          value={username}
-          placeholder="UserName"
-          onChange={(event) => {
-            setUsername(event.target.value);
-          }}
-        />
-      </div>
 
-      <div>
-        Email:
-        <input
-          type="email"
-          value={email}
-          placeholder="user@gmail.com"
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-        />
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Nome do usuario:
+          <input
+            type="text"
+            value={username}
+            placeholder="UserName"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+        </div>
 
-      <div>
-        Senha:
-        <input
-          type="password"
-          value={password}
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-        />
-      </div>
+        <div>
+          Email:
+          <input
+            type="email"
+            value={email}
+            placeholder="user@gmail.com"
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+        </div>
+
+        <div>
+          Senha:
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+        </div>
+
+        <button type="submit">Cadastrar</button>
+      </form>
     </>
   );
 }
