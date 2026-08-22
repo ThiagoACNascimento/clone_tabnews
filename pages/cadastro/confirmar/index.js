a51e976c066d4a936e6f80ce163d99298e780517
Thiago Antonio <thiagoacnascimento@gmail.com>
2026-08-22 14:58:33 -0400

    feat: add primer in `confirmRegisterPage`

--- pages/cadastro/confirmar/index.js
+++ pages/cadastro/confirmar/index.js
@@ -1,7 +1,21 @@
+import DefaultLayout from "interface/DefaultLayout";
+import { Banner } from "@primer/react";
+
 export default function ConfirmRegisterPage() {
   return (
     <>
-      <h1>Confira o seu email!</h1>
+      <DefaultLayout
+        contentWidth="small"
+        metadata={{
+          title: "Confirme seu email",
+        }}
+      >
+        <Banner
+          variant="warning"
+          title="Quase lá!"
+          description="Abra o email enviado pelo TrizCrocheting e click no link de confirmação!"
+        />
+      </DefaultLayout>
     </>
   );
 }
