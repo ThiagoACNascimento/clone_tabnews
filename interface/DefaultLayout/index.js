918e99f9fa06c273511aef54dddea050dd0cf450
Thiago Antonio <thiagoacnascimento@gmail.com>
2026-08-22 14:35:29 -0400

    feat: apply `css module` and content map

--- interface/DefaultLayout/index.js
+++ interface/DefaultLayout/index.js
@@ -1,7 +1,18 @@
 import Head from "next/head";
+import styles from "./index.module.css";
 import { PageLayout, Header, Text } from "@primer/react";
 
-export default function DefaultLayout({ children, metadata = {} }) {
+const contentWidthClasses = {
+  small: styles.smallContent,
+};
+
+export default function DefaultLayout({
+  children,
+  metadata = {},
+  contentWidth = "medium",
+}) {
+  const extraContentClassName = contentWidthClasses[contentWidth];
+
   return (
     <>
       <Head>
@@ -31,7 +42,12 @@ export default function DefaultLayout({ children, metadata = {} }) {
       </Header>
 
       <PageLayout>
-        <PageLayout.Content>{children}</PageLayout.Content>
+        <PageLayout.Content
+          width={contentWidth}
+          className={extraContentClassName}
+        >
+          {children}
+        </PageLayout.Content>
         <PageLayout.Footer divider={"line"}>
           <Text size="small"> © {new Date().getFullYear()} TrizCrocheting</Text>
         </PageLayout.Footer>
