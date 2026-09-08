56eaa1ffb1d4ee65388ade2b34cd1e4197082062
Thiago Antonio <thiagoacnascimento@gmail.com>
2026-09-08 19:17:15 -0400

    feat: add access token capture and the PATCH method to the endpoint

--- pages/cadastro/ativar/[activationTokenId].js
+++ pages/cadastro/ativar/[activationTokenId].js
@@ -1,11 +1,41 @@
 import DefaultLayout from "interface/DefaultLayout";
 import { Banner } from "@primer/react";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function ActivateUserPage() {
   const router = useRouter();
+  const activationTokenId = router.query.activationTokenId;
 
-  console.log(router.query.activationTokenId);
+  useEffect(() => {
+    if (!activationTokenId) {
+      return;
+    }
+
+    sendActivationRequest();
+
+    async function sendActivationRequest() {
+      try {
+        const response = await fetch(
+          `/api/v1/activations/${activationTokenId}`,
+          {
+            method: "PATCH",
+          },
+        );
+
+        // const activationResponseBody = await response.json();
+
+        if (response.status === 200) {
+          // retorna um sucesso
+          return;
+        }
+
+        // sinal de fracasso
+      } catch {
+        // sinal de fracasso
+      }
+    }
+  }, [activationTokenId]);
 
   return (
     <>
